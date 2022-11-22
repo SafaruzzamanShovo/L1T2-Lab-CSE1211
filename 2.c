d9c3224d49892db70d1f7a555a342a255c401a47
Shovo76 <118802492+Shovo76@users.noreply.github.com>
2022-11-22 20:29:32 +0600

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,11 @@
+// Author : Safaruzzaman Shovo
+#include <stdio.h>
+#include<math.h>
+#define pi acos(-1)
+int main() {
+    float l,r,area;
+    scanf("%f%f",&l,&r);
+    area = l*r ;
+    printf("Area of the Rectangle is %.2f\n",area);
+    return 0;
+}
