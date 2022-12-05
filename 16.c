70a3f907671203a7f8b6cf965b619d83204c5a41
Safaruzzaman Shovo <118802492+SafaruzzamanShovo@users.noreply.github.com>
2022-12-06 01:41:20 +0600

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a;
+  printf("Enter the integer : ");
+  scanf("%d",&a);
+  if(a%2) printf("Even Number\n");
+  else printf(" Odd Number\n");
+
+
+  return 0;
+}
